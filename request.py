a978d2435a97a07b9549466b058f46ff69a547c3
lucas <luquinhasmessias7@gmail.com>
2025-04-11 14:46:55 -0300

    atualizações simples

--- request.py
+++ request.py
@@ -5,7 +5,6 @@ import os
 load_dotenv(dotenv_path='C:/Users/539555318/Desktop/API-NBA/.venv/.env')
 token = os.getenv('token')
 
-token = 'a4c4340c7fef4c3dbaae344d7118d771'
 
 url = 'https://api.football-data.org/v4/competitions'
 headers = { 'X-Auth-Token': token,
@@ -21,12 +20,13 @@ for competicao in saida['competitions']:
     emblema= {competicao['emblem']}
     
 
-url_champions_league = 'https://api.football-data.org/v4/competitions/2013/standings'
 
-saida_champions = requests.get(url_champions_league, headers=headers)
+url_brasileirao = 'https://api.football-data.org/v4/competitions/2013/standings'
+
+saida_champions = requests.get(url_brasileirao, headers=headers)
 saida_champions = saida_champions.json()
 
-print(saida_champions)
 
-# for brasileirao in saida['teams']:
-#     times_brasileiros = ''
+
+for brasileirao in saida_champions['standings']:
+    print(f'times: {brasileirao['team']}')
