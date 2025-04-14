d1f9f1da6d9ce7c27d5a264ac5e81c157c1e481b
lucas <luquinhasmessias7@gmail.com>
2025-04-14 15:02:23 -0300

    correção de mensagem de bot

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import os
 from dotenv import load_dotenv
 import telebot
-
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+from request import dados_brasileirao
 
 load_dotenv(dotenv_path='C:/Users/539555318/Desktop/API-NBA/.venv/.env')
 token = os.getenv('TOKEN_TELEGRAM')
-
+brasileirao = dados_brasileirao()
 
 bot = telebot.TeleBot(token)
 bot.remove_webhook()
@@ -15,7 +16,21 @@ def start(msg:telebot.types.Message):
     bot.reply_to(msg,'Olá!')
 
 
+@bot.message_handler(['opcoes'])
+def botao(mensagem: telebot.types.Message):
+    markup = InlineKeyboardMarkup()
+    brasileirao = InlineKeyboardButton('Brasileirão', callback_data='botao1')
+    markup.add(brasileirao)
+    bot.send_message(mensagem.chat.id, 'Escolha uma opção. ', reply_markup=markup)
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == 'botao1':
+        dicionario = dados_brasileirao()
+        resposta = ""
+        for posicao, info in dicionario.items():
+            resposta += f"{posicao} {info}\n"
+        bot.send_message(call.message.chat.id, resposta)
 
 
 
