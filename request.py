da9bb897761d8d0369e8b45281467ab8909f0e4f
lucas <luquinhasmessias7@gmail.com>
2025-04-15 12:01:41 -0300

    expandindo codigo para aumentar a capacidade de busca

--- request.py
+++ request.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+import time
 
 load_dotenv(dotenv_path='C:/Users/539555318/Desktop/API-NBA/.venv/.env')
 token = os.getenv('token_api')
@@ -10,40 +11,46 @@ url = 'https://api.football-data.org/v4/competitions'
 headers = { 'X-Auth-Token': token,
            'Content-Type': 'application/json'}
 
+contador_requisicao = 0
 
 saida = requests.get(url, headers= headers)
 saida = saida.json()
 
-
+ids = []
 for competicao in saida['competitions']:
-    id_comp = f'{competicao['id']}, nome da competição: {competicao['name']}'
-    emblema= {competicao['emblem']}
-    
+    contador_requisicao += 1
+    id_comp = f'{competicao['id']}'
+    ids.append(id_comp)
+
+time.sleep(50)
+
+for i in ids:
+    url_campeonatos= f'https://api.football-data.org/v4/competitions/{i}/standings'
+    saida = requests.get(url_campeonatos, headers=headers)
+    contador_requisicao += 1
+    saida = saida.json()
+    nome_camp = f'{saida['competition']['name']}'
+
+    print(saida)
+    print('')
+    time.sleep(1)
 
 
-url_brasileirao = 'https://api.football-data.org/v4/competitions/2013/standings'
-
-saida_champions = requests.get(url_brasileirao, headers=headers)
-saida_champions = saida_champions.json()
-
-standings = saida_champions['standings'][0]['table']
+print(contador_requisicao)
 
 chaves = []
 tabela_brasileira = []
 dicionario = {}
 
-for brasileirao in standings:
-   id_team = {brasileirao['team']['id']}
-   dados_restantes = (f'{brasileirao['team']['name']}'), (f'Partidas: {brasileirao['playedGames']}'), (f'Vitorias: {brasileirao['won']}'), (f'Empates: {brasileirao['draw']}'), (f'Derrotas: {brasileirao['lost']}' )
-   colocao = (f'Posição: {brasileirao['position']}')
-   tabela_brasileira.append(dados_restantes) 
-   chaves.append(colocao)
+# for brasileirao in standings:
+#    id_team = {brasileirao['team']['id']}
+#    dados_restantes = (f'{brasileirao['team']['name']}'), (f'Partidas: {brasileirao['playedGames']}'), (f'Vitorias: {brasileirao['won']}'), (f'Empates: {brasileirao['draw']}'), (f'Derrotas: {brasileirao['lost']}' )
+#    colocao = (f'Posição: {brasileirao['position']}')
+#    tabela_brasileira.append(dados_restantes) 
+#    chaves.append(colocao)
 
-# print(tabela_brasileira)
-# print(standings)
-def dados_brasileirao():
-    for i in range(len(chaves)):
-        dicionario[chaves[i]] = tabela_brasileira[i]
-    return dicionario
-
-dados_brasileirao()
+# # print(tabela_brasileira)
+# # print(standings)
+# def dados_brasileirao():
+#     for i in range(len(chaves)):
+#         dicionario[chaves[i]] = tabela_brasileira[i]
